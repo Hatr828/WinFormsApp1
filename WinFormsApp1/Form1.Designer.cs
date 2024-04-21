3193a3408f572ffee89ae9b536f0186db843bc20
Hatr828 <hatr626@gmail.com>
2024-04-20 19:18:32 -0500

    2

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,59 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            listBox1 = new ListBox();
-            textBox1 = new TextBox();
+            panel1 = new Panel();
+            radioButton3 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            StartButton = new Button();
             button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
-            // listBox1
+            // panel1
             // 
-            listBox1.FormattingEnabled = true;
-            listBox1.ItemHeight = 20;
-            listBox1.Items.AddRange(new object[] { "cjkzklvc", "vkxzj", "zxcv", "zxcvxc", "cxzggds", "d" });
-            listBox1.Location = new Point(3, 2);
-            listBox1.Name = "listBox1";
-            listBox1.Size = new Size(246, 384);
-            listBox1.TabIndex = 0;
-            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            panel1.Controls.Add(radioButton3);
+            panel1.Controls.Add(radioButton2);
+            panel1.Controls.Add(radioButton1);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(170, 188);
+            panel1.TabIndex = 0;
+            panel1.Visible = false;
             // 
-            // textBox1
+            // radioButton3
             // 
-            textBox1.Location = new Point(255, 2);
-            textBox1.Multiline = true;
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(533, 384);
-            textBox1.TabIndex = 1;
-            textBox1.TextChanged += textBox1_TextChanged;
+            radioButton3.AutoSize = true;
+            radioButton3.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            radioButton3.Location = new Point(8, 124);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(144, 50);
+            radioButton3.TabIndex = 2;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Level 3";
+            radioButton3.UseVisualStyleBackColor = true;
+            radioButton3.CheckedChanged += radioButton3_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            radioButton2.Location = new Point(8, 68);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(144, 50);
+            radioButton2.TabIndex = 1;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Level 2";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            radioButton1.Location = new Point(8, 12);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(144, 50);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Level 1";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // StartButton
+            // 
+            StartButton.AutoSize = true;
+            StartButton.Dock = DockStyle.Bottom;
+            StartButton.Location = new Point(0, 415);
+            StartButton.Name = "StartButton";
+            StartButton.Size = new Size(1000, 73);
+            StartButton.TabIndex = 1;
+            StartButton.Text = "Start";
+            StartButton.UseVisualStyleBackColor = true;
+            StartButton.Visible = false;
+            StartButton.Click += button1_Click;
             // 
             // button1
             // 
-            button1.Location = new Point(3, 387);
+            button1.AutoSize = true;
+            button1.Dock = DockStyle.Bottom;
+            button1.Location = new Point(0, 339);
             button1.Name = "button1";
-            button1.Size = new Size(801, 70);
+            button1.Size = new Size(1000, 76);
             button1.TabIndex = 2;
-            button1.Text = "button1";
+            button1.Text = "Answer";
             button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            button1.Click += button1_Click_1;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 70F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(0, -29);
+            textBox1.MaxLength = 30;
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(988, 201);
+            textBox1.TabIndex = 3;
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            textBox1.Visible = false;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 80F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(12, 178);
+            textBox2.MaxLength = 5;
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(976, 231);
+            textBox2.TabIndex = 4;
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            textBox2.Visible = false;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 60F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(0, -2);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.Size = new Size(1000, 490);
+            textBox3.TabIndex = 5;
+            textBox3.TextAlign = HorizontalAlignment.Center;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button1);
+            ClientSize = new Size(1000, 488);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
             Controls.Add(textBox1);
-            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Controls.Add(StartButton);
+            Controls.Add(panel1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private ListBox listBox1;
-        private TextBox textBox1;
+        private Panel panel1;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        protected Button StartButton;
         private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -2,25 +2,88 @@ namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        private int level;
+        private int answer;
+        private int correctAnswers = 0;
+        private int incorrectAnswers = 0;
+        private int quastions = 1;
         public Form1()
         {
             InitializeComponent();
+            Initialize();
         }
 
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void Initialize()
+        {
+            Random rd = new Random();
+            int temp1 = rd.Next(0, 20);
+            int temp2 = rd.Next(0, 20);
+
+            panel1.Visible = true;
+            StartButton.Visible = true;
+            button1.Visible = false;
+            textBox3.Visible = false;
+            answer = temp1 * temp2;
+            textBox1.Text = temp1 + " * " + temp2;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if(listBox1.SelectedItem != null)
-                textBox1.Text = listBox1.SelectedItem.ToString();
+            foreach (var Panel in Controls.OfType<Panel>())
+            {
+                this.Controls.Remove(Panel);
+            }
+            this.Controls.Remove((Control)sender);
+            button1.Visible = true;
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            level = radioButton1.Checked ? 1 : radioButton2.Checked ? 2 : 3;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (quastions < 10)
+            {
+                Random rd = new Random();
+                int temp1 = rd.Next(0, 20);
+                int temp2 = rd.Next(0, 20);
+
+                if (string.Equals(answer.ToString(), textBox2.Text))
+                {
+                    correctAnswers += 1;
+                }
+                else
+                {
+                    incorrectAnswers += 1;
+                }
+                quastions += 1;
+                answer = temp1 * temp2 * level * level;
+                textBox2.Text = "";
+                textBox1.Text = (temp1 * level) + " * " + (temp2 * level);
+            }
+            else
+            {
+                textBox1.Visible = false;
+                textBox2.Visible = false;
+                button1.Visible = false;
+                textBox3.Visible = true;
+                textBox3.Text = "Correct answers: " + correctAnswers + "\n\n Incorrect answers: " + incorrectAnswers;
+            }
         }
     }
 }
