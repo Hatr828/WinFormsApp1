c6e3820650871a0ee0f90549325a09bf9135b01d
Hatr828 <hatr626@gmail.com>
2024-05-14 20:14:56 -0500

    2

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,20 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
+            menuStrip1 = new MenuStrip();
+            newGameToolStripMenuItem = new ToolStripMenuItem();
+            lbFildLoadToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            dateTimePicker1 = new DateTimePicker();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { newGameToolStripMenuItem, lbFildLoadToolStripMenuItem, exitToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(722, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            newGameToolStripMenuItem.Size = new Size(95, 24);
+            newGameToolStripMenuItem.Text = "New game";
+            newGameToolStripMenuItem.Click += newGameToolStripMenuItem_Click;
+            // 
+            // lbFildLoadToolStripMenuItem
+            // 
+            lbFildLoadToolStripMenuItem.Name = "lbFildLoadToolStripMenuItem";
+            lbFildLoadToolStripMenuItem.Size = new Size(99, 24);
+            lbFildLoadToolStripMenuItem.Text = "Lb fild load";
+            lbFildLoadToolStripMenuItem.Click += lbFildLoadToolStripMenuItem_Click;
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(47, 24);
+            exitToolStripMenuItem.Text = "Exit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(292, 114);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(250, 27);
+            dateTimePicker1.TabIndex = 1;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(722, 436);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(menuStrip1);
             Name = "Form1";
             Text = "Form1";
             Paint += Form1_Paint;
-            MouseClick += Form1_MouseClick;
+            KeyPress += Form1_KeyPress;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem newGameToolStripMenuItem;
+        private ToolStripMenuItem lbFildLoadToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private DateTimePicker dateTimePicker1;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -12,35 +12,142 @@ namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
-        Point points;
+        private const int CellSize = 20;
+        private const int MazeWidth = 34;
+        private const int MazeHeight = 20;
+
+        private Point playerPosition;
+
+        private bool[,] maze;
 
         public Form1()
         {
             InitializeComponent();
-
+            GenerateMaze();
+            ResetPlayerPosition();
         }
 
-        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        private void GenerateMaze()
         {
-            points = new Point(e.X,e.Y);
-            Invalidate();
+            maze = new bool[MazeWidth, MazeHeight];
+            Random rand = new Random();
+            for (int x = 0; x < MazeWidth; x++)
+            {
+                for (int y = 0; y < MazeHeight; y++)
+                {
+                    maze[x, y] = rand.Next(5) > 0;
+                }
+            }
+
+            maze[MazeWidth - 1, rand.Next(MazeHeight)] = true;
+        }
+
+        private void ResetPlayerPosition()
+        {
+            playerPosition = new Point(0, MazeHeight / 2);
+            maze[0, MazeHeight / 2] = true;
+            Refresh();
+        }
+
+        private void TryMovePlayer(int dx, int dy)
+        {
+            int newX = playerPosition.X + dx;
+            int newY = playerPosition.Y + dy;
+
+            if (newX >= 0 && newX < MazeWidth && newY >= 0 && newY < MazeHeight && maze[newX, newY])
+            {
+                playerPosition = new Point(newX, newY);
+                Refresh();
+                if (playerPosition.X == MazeWidth - 1 && maze[playerPosition.X, playerPosition.Y])
+                {
+                    MessageBox.Show("Вы победили!");
+                    GenerateMaze();
+                    ResetPlayerPosition();
+                }
+            }
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            Graphics g = this.CreateGraphics();
-            Random rd = new Random();
-            var brushes = new Brush[]
+            Graphics g = e.Graphics;
+
+            for (int x = 0; x < MazeWidth; x++)
             {
-                Brushes.AliceBlue,
-                Brushes.Aqua,
-                Brushes.Beige,
-                Brushes.BurlyWood,
-                Brushes.DarkOrchid,
-                Brushes.LightGreen
-            };
-            int circleSize = rd.Next(10, 70);
-                g.FillEllipse(brushes[rd.Next(0, brushes.Length)], points.X, points.Y, circleSize, circleSize);
+                for (int y = 0; y < MazeHeight; y++)
+                {
+                    Brush brush = maze[x, y] ? Brushes.White : Brushes.Black;
+                    g.FillRectangle(brush, x * CellSize, y * CellSize, CellSize, CellSize);
+                }
+            }
+
+            g.FillEllipse(Brushes.Red, playerPosition.X * CellSize, playerPosition.Y * CellSize, CellSize, CellSize);
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.Equals('w'))
+            {
+
+                TryMovePlayer(0, -1);
+            }
+            else if (e.KeyChar.Equals('s'))
+            {
+                TryMovePlayer(0, 1);
+            }
+            else if (e.KeyChar.Equals('a'))
+            {
+                TryMovePlayer(-1, 0);
+            }
+            else if (e.KeyChar.Equals('d'))
+            {
+                TryMovePlayer(1, 0);
+            }
+
+
+
+
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GenerateMaze();
+            ResetPlayerPosition();
+        }
+
+        private void lbFildLoadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            maze = new bool[MazeWidth, MazeHeight];
+            try
+            {
+                string[] lines = File.ReadAllLines("Lb.text");
+                if (lines.Length == MazeHeight)
+                {
+                    for (int y = 0; y < MazeHeight; y++)
+                    {
+                        string line = lines[y];
+                        if (line.Length == MazeWidth)
+                        {
+                            for (int x = 0; x < MazeWidth; x++)
+                            {
+                                maze[x, y] = line[x] == '1';
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return;
+            }
+            ResetPlayerPosition();
+            Refresh();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
+
 }
